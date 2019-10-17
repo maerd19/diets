44985ee99fc5edadb259e6f2a98910760d3e78f1
Patricia <pattcaballero@gmail.com>
2019-10-16 22:12:35 -0500

    Model Objectives :tada:

--- models/Objectives.js
+++ models/Objectives.js
@@ -2,4 +2,34 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-module.exports();
+const objectivesShema = new Schema(
+  {
+    gender: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: true
+    },
+    exercise: {
+      type: Boolean,
+      required: true
+    },
+    objectives: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Ojectives", objectivesShema);
