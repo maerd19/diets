22558d46162544cc7190b1c491337ff9e94d3ff8
Patricia <pattcaballero@gmail.com>
2019-10-19 13:57:06 -0500

    Days view

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const session      = require("express-session");
-const MongoStore   = require("connect-mongo")(session);
-const passport     = require("./helpers/passport");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("./helpers/passport");
 
 mongoose
-  .connect('mongodb://localhost/module2', {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect("mongodb://localhost/module2", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   // .connect(process.env.DB, {
   //   useNewUrlParser: true,
   //   useUnifiedTopology: true
   // })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -53,30 +60,30 @@ app.use(passport.session());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
-const index = require('./routes/index');
-const auth = require('./routes/auth');
-const objectives = require('./routes/objectives');
+const index = require("./routes/index");
+const auth = require("./routes/auth");
+const objectives = require("./routes/objectives");
+const days = require("./routes/days");
 
-app.use('/', index);
-app.use('/', auth);
-app.use('/', objectives);
-
+app.use("/", index);
+app.use("/", auth);
+app.use("/", objectives);
+app.use("/", days);
 
 module.exports = app;
